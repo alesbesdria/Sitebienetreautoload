54418a1e19f8bb0b45672df72e2d512830c3cdd5
alesbesdria <melissa@meirsman.fr>
2024-11-04 01:51:48 +0100

    newlastregister

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,8 @@
 <?php
-define("ROOT", $_SERVER["DOCUMENT_ROOT"]."..");
-require_once ROOT."/vendor/autoload.php";
+define("ROOT", $_SERVER["DOCUMENT_ROOT"] . "..");
+require_once ROOT . "/vendor/autoload.php";
 
-use Admin\Controllers\ControllerText;
 use Admin\Router\Router;
-use Admin\Models\Logs;
-use Admin\Controllers\ControllerProfil;
-
 
 $app = new Router();
 
