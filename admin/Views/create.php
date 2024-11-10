9cbf67455bd7bb74b8ce729fad74269cd4722141
alesbesdria <melissa@meirsman.fr>
2024-11-10 14:20:04 +0100

    newlastregister

--- admin/Views/create.php
+++ admin/Views/create.php
@@ -3,19 +3,19 @@
 <form action="/admin/user/store" method="POST">
         <div>
             <label for="user_firstname">Prénom :</label>
-            <input type="text" id="user_firstname" name="user_firstname" required>
+            <input type="text" id="user_firstname" name="user_firstname" >
         </div>
         <div>
             <label for="user_lastname">Nom :</label>
-            <input type="text" id="user_lastname" name="user_lastname" required>
+            <input type="text" id="user_lastname" name="user_lastname" >
         </div>
         <div>
             <label for="user_mail">Email :</label>
-            <input type="email" id="user_mail" name="user_mail" required>
+            <input type="email" id="user_mail" name="user_mail" >
         </div>
         <div>
             <label for="id_role">Rôle :</label>
-            <select name="id_role" id="id_role" required>
+            <select name="id_role" id="id_role" >
                 <?php foreach ($roles as $role): ?>
                     <option value="<?= $role->id_role ?>"><?= $role->role_name ?></option>
                 <?php endforeach; ?>
@@ -23,11 +23,11 @@
         </div>
         <div>
             <label for="user_mdp">Mot de passe :</label>
-            <input type="password" id="user_mdp" name="user_mdp" required>
+            <input type="password" id="user_mdp" name="user_mdp" >
         </div>
         <div>
             <label for="confMdp">Confirmation du mot de passe :</label>
-            <input type="password" id="confMdp" name="confMdp" required>
+            <input type="password" id="confMdp" name="confMdp" >
         </div>
         <div>
             <button type="submit">Ajouter l'utilisateur</button>
